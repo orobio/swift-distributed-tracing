f327dc8ae44d046799218d6ef98655d27aad93e8
Manolo van Ee <manolo.vanee@gmail.com>
2024-08-21 11:20:23 +0200

    Add Package@swift-6.0.swift
    Copied from Package.swift and updated swift-tools-version to 6.0.

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,48 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "swift-distributed-tracing",
+    products: [
+        .library(name: "Instrumentation", targets: ["Instrumentation"]),
+        .library(name: "Tracing", targets: ["Tracing"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-service-context.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
+    targets: [
+        // ==== --------------------------------------------------------------------------------------------------------
+        // MARK: Instrumentation
+
+        .target(
+            name: "Instrumentation",
+            dependencies: [
+                .product(name: "ServiceContextModule", package: "swift-service-context"),
+            ]
+        ),
+        .testTarget(
+            name: "InstrumentationTests",
+            dependencies: [
+                .target(name: "Instrumentation"),
+            ]
+        ),
+
+        // ==== --------------------------------------------------------------------------------------------------------
+        // MARK: Tracing
+
+        .target(
+            name: "Tracing",
+            dependencies: [
+                .product(name: "ServiceContextModule", package: "swift-service-context"),
+                .target(name: "Instrumentation"),
+            ]
+        ),
+        .testTarget(
+            name: "TracingTests",
+            dependencies: [
+                .target(name: "Tracing"),
+            ]
+        ),
+    ]
+)
